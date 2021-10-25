bfd708a5a43f99c7ea4ed7768dc8431252681b3b
SSJenny90 <samuel.scott.jennings@gmail.com>
2021-10-25 19:40:58 +1030

    import in __init__

--- geoage_calculator/__init__.py
+++ geoage_calculator/__init__.py
@@ -4,3 +4,5 @@ __docformat__ = 'numpy'
 __version__ = "1.0.0"
 __version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
 __status__ = 'Stable'
+
+from geoage_calculator import geo_age_lookup, age_range_lookup
